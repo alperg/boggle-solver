d57d7664c09c9cca8dab6d05bf5deef9839d7a9a
Alper Gokcehan <mail@alperg.com>
2020-01-05 22:58:21 -0500

    Updates

---
+++ precache-manifest.58353e045151617e3a11d3634109faf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b859ccc1d71cb53e387fb2fed46ce08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "706768acfacbfdaec8d4",
+    "url": "/static/css/main.5966b1e5.chunk.css"
+  },
+  {
+    "revision": "01af0c3952eb496321b2",
+    "url": "/static/js/2.e3974c25.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.e3974c25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "706768acfacbfdaec8d4",
+    "url": "/static/js/main.52f3ddfd.chunk.js"
+  },
+  {
+    "revision": "73650376a800d5a6db4c",
+    "url": "/static/js/runtime-main.d69476b9.js"
+  }
+]);
